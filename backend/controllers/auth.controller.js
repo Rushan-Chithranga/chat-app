b4ce9d8a9078b1ed4579bdd65d8c83b8a74fe346
Rushan Chithranga <rushanchith123@gmail.com>
2024-11-24 12:45:33 +0530

    User auth done

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -1,11 +1,86 @@
+import bcrypt from "bcryptjs";
+import User from "../models/user.model.js";
+import genaretTokenAndSetCookie from "../utils/generateToken.js";
+
 export const login = async (req, res) => {
   try {
-    const { fullName, username, password, confirmPassword, gender } = req.body;
-  } catch (error) {}
+    const { username, password } = req.body;
+    const user = await User.findOne({ username });
+    const isPassword = await bcrypt.compare(password, user?.password || "");
+
+    if (!user || !isPassword) {
+      return res.status(400).json({ error: "Invalid username or password" });
+    }
+
+    genaretTokenAndSetCookie(user._id, res);
+
+    res.status(200).json({
+      _id: user._id,
+      fullName: user.fullName,
+      username: user.username,
+      profilePic: user.profilePic,
+      succses: "User Signin successfully",
+    });
+  } catch (error) {
+    console.log("Error in signup controller", error.message);
+    res.status(500).json({ error: "Internal server error" });
+  }
 };
-export const signup = (req, res) => {
-  console.log("Signup user.....");
+
+export const signup = async (req, res) => {
+  try {
+    const { fullName, username, password, confirmPassword, gender } = req.body;
+
+    if (password !== confirmPassword) {
+      return res.status(400).json({ error: "Password don't match" });
+    }
+
+    const user = await User.findOne({ username });
+
+    if (user) {
+      return res.status(400).json({ error: "Username already exists" });
+    }
+
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(password, salt);
+
+    const boyProfilePic = `https://avatar.iran.liara.run/public/boy?username=${username}`;
+    const girlProfilePic = `https://avatar.iran.liara.run/public/girld?username=${username}`;
+
+    const newUser = new User({
+      fullName,
+      username,
+      password: hashedPassword,
+      gender,
+      profilePic: gender === "male" ? boyProfilePic : girlProfilePic,
+    });
+
+    if (newUser) {
+      genaretTokenAndSetCookie(newUser._id, res);
+      await newUser.save();
+
+      res.status(200).json({
+        _id: newUser._id,
+        fullName: newUser.fullName,
+        username: newUser.username,
+        profilePic: newUser.profilePic,
+        gender: newUser.gender,
+        succses: "User Signup successfully",
+      });
+    } else {
+      return res.status(400).json({ error: "Invalid user data" });
+    }
+  } catch (error) {
+    console.log("Error in signup controller", error.message);
+    res.status(500).json({ error: "Internal server error" });
+  }
 };
 export const logout = (req, res) => {
-  console.log("Logout user.....");
+  try {
+    res.cookie("jwt", "", { maxAge: 0 });
+    res.status(200).json({ message: "Logged out successfully" });
+  } catch (error) {
+    console.log("Error in signup controller", error.message);
+    res.status(500).json({ error: "Internal server error" });
+  }
 };
