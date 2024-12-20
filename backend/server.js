114fa68c87dd4721f42fac67a053d83a63c7d5ff
Rushan Chithranga <rushanchith123@gmail.com>
2024-12-20 19:54:32 +0530

    #1 create message backend

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,11 @@
 import express from "express";
 import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
+
 import authRoutes from "./routes/auth.routes.js";
+import messageRoutes from "./routes/message.routes.js";
+import userRoutes from "./routes/user.routes.js";
+
 import connectToMongoDB from "./db/connectToMongoDB.js";
 
 const app = express();
@@ -9,12 +14,16 @@ dotenv.config();
 const PORT = process.env.PORT || 5000;
 
 app.use(express.json());
+app.use(cookieParser());
 
 app.get("/", (req, res) => {
   res.send("hello world!");
 });
 
 app.use("/api/auth", authRoutes);
+app.use("/api/messages", messageRoutes);
+app.use("/api/users", userRoutes);
+
 app.listen(PORT, () => {
   connectToMongoDB();
   console.log("Server Running on port " + PORT);
